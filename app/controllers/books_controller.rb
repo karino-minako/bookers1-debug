00d94833c40983d1de4a84330ff0f655f953d034
vagrant <vagrant@localhost.localdomain>
2020-05-16 05:18:54 +0000

    [Finish] task-1

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,7 @@
 class BooksController < ApplicationController
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
+  
+  def top
+  end
 
   # GET /books
   # GET /books.json
@@ -24,9 +26,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to @book, notice: 'Book was successfully created.'
+       flash[:notice] = 'Book was successfully created.'
+       redirect_to book_path(@book)
     else
-      render :index
+       @books = Book.all
+       render :index
     end
   end
 
@@ -34,26 +38,25 @@ class BooksController < ApplicationController
   # PATCH/PUT /books/1.json
   def update
     @book = Book.find(params[:id])
-    if @book.update()
-      redirect_to @book, notice: 'Book was successfully updated.'
+    if @book.update(book_params)
+       flash[:notice] = 'Book was successfully updated.'
+       redirect_to book_path(@book)
     else
-      render :edit
+       render :edit
     end
   end
 
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
+    @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_url, notice: 'Book was successfully destroyed.'
+    flash[:notice] = 'Book was successfully destroyed.'
+    redirect_to books_path
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_book
-      @book = Book.find(params[:id])
-    end
-
+   
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
       params.require(:book).permit(:title, :body)
